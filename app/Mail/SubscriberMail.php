22a6949a927b665bb4f35e376b174f82ec8113f2
Machina maiki <machina@localhost.localdomain>
2021-04-28 20:18:30 +0300

    test changed mail

--- app/Mail/SubscriberMail.php
+++ app/Mail/SubscriberMail.php
@@ -29,6 +29,6 @@ class SubscriberMail extends Mailable
      */
     public function build()
     {
-        return $this->markdown('emails.subscriber')->with(['mailerInfo' => $this->mailerInfo]);
+        return $this->view('emails.subscriberer', ['mailerInfo' => $this->mailerInfo]);
     }
 }
